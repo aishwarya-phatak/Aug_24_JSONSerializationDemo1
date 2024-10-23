6e337aed052b0a78b82edada6853e0ea56b59ebe
aishwarya <ashp17star@gmail.com>
2024-10-23 12:11:51 +0530

    Binding Data With Table View

---
+++ Aug_24_JSONSerializationDemo1/UserTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserTableViewCell.swift
+//  Aug_24_JSONSerializationDemo1
+//
+//  Created by Vishal Jagtap on 23/10/24.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
